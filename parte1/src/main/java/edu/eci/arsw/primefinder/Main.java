e21252b09846e8306b6848d47cad1b0dd55c509b
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2020-01-28 21:57:59 -0500

    Salida Main primos

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -17,13 +17,22 @@ public class Main {
 			pft2.pausar();
 			pft3.pausar();
 		} catch (Exception e){}
-		System.out.println(pft1.getPrimes().size());
-		System.out.println(pft2.getPrimes().size());
-		System.out.println(pft3.getPrimes().size());
+		System.out.println("De 0 a 10000000: "+ pft1.getPrimes().size());
+		System.out.println("De 10000000 a 20000000: "+ pft2.getPrimes().size());
+		System.out.println("De 20000000 a 30000000: "+ pft3.getPrimes().size());
 		c.format("\nPresionar Enter para continuar.\n");
 		c.readLine();
 		pft1.reaunudar();
 		pft2.reaunudar();
 		pft3.reaunudar();
+		boolean vivos = true;
+        while (vivos==true){
+            if(pft1.isAlive() || pft2.isAlive() || pft3.isAlive()){
+                vivos = true;
+            }else{vivos=false;}
+		}
+		System.out.println("De 0 a 10000000: "+ pft1.getPrimes().size());
+		System.out.println("De 10000000 a 20000000: "+ pft2.getPrimes().size());
+		System.out.println("De 20000000 a 30000000: "+ pft3.getPrimes().size());
 	}	
 }
